1afda551561bfb6eadeedfcbf091ddedf4fec0c0
Oskari Kosonen <o.kosonen@gmail.com>
2023-12-04 17:14:22 +0200

    Day 4

---
+++ src/test/test_day04.py
@@ -0,0 +1,47 @@
+"""Advent of Code 2023 - Unit tests for day 4 tasks"""
+from ..day04 import part_one, part_two
+from .. import util
+
+
+def test_part_one():
+    """
+    Test function for part_one.
+
+    This function tests the implementation of the part_one function by providing example
+    data and asserting the expected output.
+
+    Returns:
+        None
+    """
+    example_data = [
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+    ]
+    assert part_one(example_data) == 13
+    assert part_one(util.get_lines('day04')) == 21088
+
+
+def test_part_two():
+    """
+    Test function for the part_two() function.
+
+    This function tests the part_two() function using example data and the actual input data.
+    It asserts that the output of part_two() matches the expected values.
+
+    Returns:
+        None
+    """
+    example_data = [
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+    ]
+    assert part_two(example_data) == 30
+    assert part_two(util.get_lines('day04')) == 6874754
